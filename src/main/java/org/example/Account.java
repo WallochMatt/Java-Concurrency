7a3ad41be2bb852fa4e07c5bbff3638c4984106e
WallochMatt <matthewrwalloch@gmail.com>
2022-11-10 11:00:34 -0600

    feat: Created Account and Simulation Classes + Created a method to withdraw money from account

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Account {
+    private int balance;
+    private int amountWithdrawn;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getAmountWithdrawn() {
+        return amountWithdrawn;
+    }
+
+    public void setAmountWithdrawn(int amountWithdrawn) {
+        this.amountWithdrawn = amountWithdrawn;
+    }
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int amount){
+        if(amount <= this.balance){
+            this.balance -= amount;
+            this.amountWithdrawn += amount;
+            System.out.println(amount + " withdrawn. New balance " + this.balance);
+        }
+        else{
+            System.out.println("Insufficient funds. Total withdrawn: " + this.amountWithdrawn);
+        }
+    }
+}
---
+++ src/main/java/org/example/Simulation.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Simulation {
+
+    public void runSim(){
+        System.out.println("Running simulation");
+        Account myAccount = new Account(1000);
+        myAccount.withdraw(200);
+    }
+}
